ca33ce23bf66732e222b16c6fc4bf2480ac3c60a
dzh <411788973@qq.com>
2020-04-15 18:23:31 +0800

    把版本号展示Activity上

--- app/src/main/java/com/stormdzh/ffmpegdzh/MainActivity.java
+++ app/src/main/java/com/stormdzh/ffmpegdzh/MainActivity.java
@@ -3,12 +3,17 @@ package com.stormdzh.ffmpegdzh;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.stormdzh.libaudio.util.TestJni;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MFFMPEG";
+
+    private TextView tvTest;
+    private TextView tvVersion;
+
     private TestJni mTestJni;
 
     @Override
@@ -16,10 +21,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvTest = findViewById(R.id.tvTest);
+        tvVersion = findViewById(R.id.tvVersion);
+
         mTestJni = new TestJni();
 
 
         Log.i(TAG, "getStringFromJNI:" + mTestJni.getStringFromJNI());
+        tvTest.setText(mTestJni.getStringFromJNI());
         Log.i(TAG, "获取到ffmpeg版本号:" + mTestJni.getTestFFmpeg());
+        tvVersion.setText("获取到ffmpeg版本号:" + mTestJni.getTestFFmpeg());
     }
+
 }
